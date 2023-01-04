740aa0c56b5256ca633ba8046bbccd302f9e1ae4
jeremy-lyoko <71481878+jeremy-lyoko@users.noreply.github.com>
2023-01-04 13:38:48 +0800

    Delete day11Game directory

--- day11Game/src/App.java
+++
@@ -1,9 +0,0 @@
-import ui.GameJFrame;
-
-public class App {
-    public static void main(String[] args) {
-        //new LoginJFrame();
-        //new RegisterJFrame();
-        new GameJFrame();
-    }
-}
